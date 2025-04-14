dd799b7fd44d7be5e6bc5a69f6d065e2e4085c14
Mehmet <mehmetzahir5834@gmail.com>
2025-04-14 15:25:18 +0300

    Updated Project

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context=new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience p)
+        {
+            context.Experiences.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var experience = context.Experiences.Find(id);
+            context.Experiences.Remove(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var experience = context.Experiences.Find(id);
+            return View(experience);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience p)
+        {
+            context.Experiences.Update(p);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
         }
     }
 }
---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public IActionResult ChangeIsRead(int id)
+        {
+            var message = context.Messages.Find(id);
+            if(message.IsRead == true)
+            {
+                message.IsRead = false;
+            }
+            else
+            {
+                message.IsRead = true;
+            }
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult DeleteMessage(int id)
+        {
+            var message = context.Messages.Find(id);
+            context.Messages.Remove(message);
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult MessageDetail(int id)
+        {
+            var value = context.Messages.Find(id);
+            return View(value);
+        }
+    }
+}
